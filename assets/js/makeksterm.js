9409718f622b79bf443a5e96e510aaefed9cfd84
Cade Brown <me@cade.site>
2022-01-26 00:47:53 -0500

    updated terminal

--- assets/js/makeksterm.js
+++ assets/js/makeksterm.js
@@ -18,26 +18,41 @@ function makeksterm(id) {
         // Create a locals array
         let locals = ks.make_dict()
         let ct = 0
+
+
+        // whether or not we are still initializing
+        let isearly = true
+        let write = text => {
+            text = $.terminal.escape_brackets(text)
+            term.echo(text)
+        }
         // Create a terminal from the jQuery terminal plugin
         let term = $('#' + id).terminal(function (cmd, term) {
+
+
             if (!term.has_init) {
                 term.has_init = true
 
                 // Add callbacks to write IO
-                ks._write_stdout.push(text => term.echo(text))
-                ks._write_stderr.push(text => term.echo(text))
+                ks._write_stdout.push(text => write(text))
+                ks._write_stderr.push(text => write(text))
             }
             // Get a hash of the IO objects so we can tell if something has been printed
             let hash = ks.iohash()
             let this_ct = ct
+            window.T = term
+            
+            //cmd = term.get_command()
+            //console.log("CMD", cmd)
             
             // Now, evaluate the command
             let res = ks.eval(cmd, "<inter-" + ct + ">", locals)
 
             if (res) {
-                if (ks.iohash() == hash) {
+                if (!isearly && ks.iohash() == hash) {
                     // Nothing has been printed in the mean time, so print the result
-                    term.echo(ks.get_repr(res))
+                    let r = ks.get_repr(res)
+                    write(r)
                 }
                 ks.decref(res)
             } else {
@@ -52,18 +67,52 @@ function makeksterm(id) {
             prompt: ">>> ",
         })
 
+        // disable formatting
+        //$.terminal.defaults.formatters = []
+        $.terminal.defaults.formatters.unshift([/(^|[^\x08]|\r\n)\x08/g, '', true]);
+
+        write("")
+        write("### kscript web terminal")
+        write("#")
+        write("# overall, similar to Python syntax, except:")
+        write("#   * no relevant whitespace (use 'if x {...}')")
+        write("#   * different naming scheme (true, false, inf, nan)")
+        write("#   * use 'obj.__attr' for list of all attributes")
+        write("#")
+        write("# things to try:")
+        write("#   * '2**100'")
+        write("#   * 'm.isprime(2**7 - 1)'")
+        write("#   * 'm.zeta(2)'")
+        write("#   * 'nx.fft.fft([[1, 2], [3, 4]])'")
+        write("#")
+        write("# quick links:")
+        write("#   * https://docs.kscript.org/#Syntax")
+        write("#   * https://docs.kscript.org/#Functions")
+        write("#   * https://docs.kscript.org/#Modules")
+        write("#")
+        write("# author: Cade Brown https://cade.site/about")
+        write("#")
+        write("###")
+        write("")
+
+        term.exec("import m")
+        term.exec("import nx")
+        term.exec("import os")
+        term.exec("import util")
 
         // Now, execute the lines in the URL, if there were any
         var a = location.href;
         var qi = a.indexOf("?");
         if (qi > 0) {
-        var qt = decodeURIComponent(a.substring(qi+1))
-        var qtl = qt.split(/\n|\\\\/)
-        for (var i in qtl) {
-            var line = qtl[i]
-            term.exec(line)
+            var qt = decodeURIComponent(a.substring(qi+1))
+            var qtl = qt.split(/\n|\\\\/)
+            for (var i in qtl) {
+                var line = qtl[i]
+                term.exec(line)
+            }
         }
-        }
+
+        isearly = false
 
         // Return the terminal object
         return term
